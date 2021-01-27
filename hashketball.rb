1b67bef37a547677bd46c25dfae8c1e5358212e8
Diana Liao <dijali@gmail.com>
2021-01-27 19:04:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -198,9 +198,9 @@ end
 
 def player_numbers (team)
   if game_hash[:home][:team_name] == team 
-    game_hash[:home][:players].map {|n| 
+    game_hash[:home][:players].map {|n| n.values[:number]}
   elsif game_hash[:away][:team_name] == team 
-    game_hash[:away][:colors]
+    game_hash[:home][:players].map {|n| n.values[:number]}
   else
     puts "Team not found"
   end 
